b8bf82a9be9722630d528185c2f9014eed87c755
Thomas Whittle <tewhittle@gmail.com>
2021-03-16 22:18:45 -0400

    Add bio to main page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,29 @@
 function Main() {
     return(
-        <h1>
-            Hello
-        </h1>
+        <div class="cf ph7-ns ">
+            <div className="fl w-75 w-50-ns ph6">
+                <img src="14233154_10154471257388498_8193920747199696841_n.jpg" class="mw-25 f5 measure br-100 ba b--black-10 h4 w4" alt="" />
+                    
+            </div>
+            
+            <div className="fl w-75 w-50-ns mh5">
+                <p class="measure lh-copy">
+                    I'm a full-stack web developer for hire, living in Columbus, Ohio.
+                    In addition to
+                    attending a web developer bootcamp at Ohio State University,
+                    I continue to seek education in other languages that catch my
+                    fancy such as C#, Python, and C++. I also have an interest
+                    in physical computing, so I love using Python to do cool stuff
+                    with my Raspberry Pi. 
+                    <hr />
+
+                    Outside of my professional life, I love cooking, Formula 1,
+                    and going to the gym. My dream is to get a job in motorsports
+                    (whether that's in web development or otherwise) and I'd love
+                    to relocate.
+                </p>
+            </div>
+        </div>
     )
 }
 
